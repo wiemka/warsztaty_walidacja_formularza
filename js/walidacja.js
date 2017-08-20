6c6de9601963a8f2bd5652e6c8d6ccfe5d7c6986
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-20 17:02:06 +0200

    walidacja pierwszego pola

--- js/walidacja.js
+++ js/walidacja.js
@@ -1,2 +1,12 @@
 'use strict';
 
+var wyslijBtn = document.getElementById("wyślij");
+
+function waliduj(){
+    if(document.getElementById("imie").value.trim() == ""){
+        alert("Wypełnij imie i nazwisko");
+    }
+    
+}
+
+wyslijBtn.addEventListener("click", waliduj);
