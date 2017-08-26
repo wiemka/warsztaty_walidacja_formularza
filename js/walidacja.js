ad2165e0a5156acd2fdc20c864719829836bb283
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 09:28:06 +0200

    dodanie alertu email i 1.checkbox

--- js/walidacja.js
+++ js/walidacja.js
@@ -2,11 +2,22 @@
 
 var wyslijBtn = document.getElementById("wyślij");
 
-function waliduj(){
-    if(document.getElementById("imie").value.trim() == ""){
+function stanCheckbox {
+    alert("działa");
+}
+
+function waliduj() {
+    if (document.getElementById("imie").value.trim() == "") {
         alert("Wypełnij imie i nazwisko");
     }
-    
+    if (document.getElementById("email").value.trim() == "") {
+        alert("Wypełnij e-mail");
+    }
+    if (!document.getElementById("zgoda_1").checked) {
+        alert("Zaznacz pierwszą zgodę")
+    }
+
+
 }
 
 wyslijBtn.addEventListener("click", waliduj);
