d78db0af2cdc7fe7dcbcee257750900ecc74f4dc
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 09:50:09 +0200

    lepsza wersja this.checked

--- js/walidacja.js
+++ js/walidacja.js
@@ -9,13 +9,8 @@ function stanCheckbox() {
 
     // this.checked - zwraca true/false
 
-    if (this.checked) {
-        zgoda_marketingowa_1.checked = true;
-        zgoda_marketingowa_2.checked = true;
-    } else {
-        zgoda_marketingowa_1.checked = false;
-        zgoda_marketingowa_2.checked = false;
-    }
+    zgoda_marketingowa_1.checked = this.checked;
+    zgoda_marketingowa_2.checked = this.checked;
 }
 
 //document.getElementById(zgoda_all).onchange = stanCheckbox;
